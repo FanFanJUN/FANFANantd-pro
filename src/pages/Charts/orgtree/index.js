fb9711da3758fb58f36bb807bc2788e532c478f5
Cai Li <lc18280689542@gmail.com>
2019-12-25 16:11:30 +0800

    echartstree全屏显示

--- src/pages/Charts/orgtree/index.js
+++ src/pages/Charts/orgtree/index.js
@@ -3,6 +3,7 @@ import { Card } from 'antd';
 import echarts from 'echarts/lib/echarts';
 import 'echarts/lib/chart/tree';
 import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/toolbox';
 import 'echarts/lib/component/title';
 import { toThousands } from '@/utils/utils';
 
@@ -34,6 +35,7 @@ class OrgTree extends React.Component {
     this.state = {
       // dataLoading: true,
       // dataSource: [],
+      isFullScreen: false,
     };
   }
 
@@ -47,12 +49,99 @@ class OrgTree extends React.Component {
     // });
   }
 
+  fullScreen = () => {
+    if (!this.state.isFullScreen) {
+      this.requestFullScreen();
+      this.setState({ isFullScreen: true });
+    } else {
+      this.exitFullscreen();
+      this.setState({ isFullScreen: false });
+    }
+  };
+
+  // 進入全屏
+  requestFullScreen = () => {
+    console.log('requestFullScreen');
+    const de = document.getElementById('main');
+    if (de.requestFullscreen) {
+      de.requestFullscreen();
+    } else if (de.mozRequestFullScreen) {
+      de.mozRequestFullScreen();
+    } else if (de.webkitRequestFullScreen) {
+      de.webkitRequestFullScreen();
+    }
+  };
+
+    // 退出全屏
+    exitFullscreen = () => {
+      console.log('exitFullscreen');
+      const de = document;
+      if (de.exitFullscreen) {
+        de.exitFullscreen();
+      } else if (de.mozCancelFullScreen) {
+        de.mozCancelFullScreen();
+      } else if (de.webkitCancelFullScreen) {
+        de.webkitCancelFullScreen();
+      }
+    };
+
   inittree = (data) => {
     const myChart = echarts.init(document.getElementById('main'), 'macarons');
     const option = this.getOption(data);
     if (option && typeof option === 'object') {
       myChart.setOption(option);
     }
+    // https://zhuanlan.zhihu.com/p/37484972 Echarts 骚操作自适应页面
+    window.addEventListener('resize', () => {
+      myChart.resize();
+    });
+    document.addEventListener('fullscreenchange', () => {
+      if (document.fullscreen) {
+        myChart.setOption({
+          toolbox: {
+            feature: {
+              myFull: {
+                show: true,
+                title: '退出全屏',
+                icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
+                onclick: () => {
+                  this.fullScreen();
+                },
+              },
+            },
+          },
+        });
+      } else {
+        myChart.setOption({
+          toolbox: {
+            feature: {
+              myFull: {
+                show: true,
+                title: '全屏查看',
+                icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
+                onclick: () => {
+                  this.fullScreen();
+                },
+              },
+            },
+          },
+        });
+      }
+    });
+
+    document.addEventListener('msfullscreenchange', () => {
+      fullscreenState.innerHTML = (document.msFullscreenElement) ? '' : 'not ';
+    }, false);
+
+    document.addEventListener('mozfullscreenchange', () => {
+      fullscreenState.innerHTML = (document.mozFullScreen) ? '' : 'not ';
+    }, false);
+
+    document.addEventListener('webkitfullscreenchange', () => {
+      if (document.webkitIsFullScreen === false) { console.log(111); } else {
+        console.log(222);
+      }
+    }, false);
   }
   getOption = (data) => {
     // const curve = '---------------------------------------';
@@ -103,7 +192,6 @@ class OrgTree extends React.Component {
       },
       toolbox: {
         show: true,
-        right: '10px',
         feature: {
           dataView: { show: true, readOnly: false },
           restore: { show: false },
@@ -113,24 +201,25 @@ class OrgTree extends React.Component {
             title: '全屏查看',
             icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
             onclick: () => {
-              const element = document.getElementById('main');
-              const echart = echarts.init(element, 'light');
-              window.onresize = () => {
-                echart.resize();
-              };
-              // IE 10及以下ActiveXObject
-              if (window.ActiveXObject) {
-                const WsShell = new ActiveXObject('WScript.Shell');
-                WsShell.SendKeys('{F11}');
-              } else if (element.requestFullScreen) { // HTML W3C 提议
-                element.requestFullScreen();
-              } else if (element.msRequestFullscreen) { // IE11
-                element.msRequestFullScreen();
-              } else if (element.webkitRequestFullScreen) { // Webkit (works in Safari5.1 and Chrome 15)
-                element.webkitRequestFullScreen();
-              } else if (element.mozRequestFullScreen) { // Firefox (works in nightly)
-                element.mozRequestFullScreen();
-              }
+              this.fullScreen();
+              // const element = document.getElementById('main');
+              // const echart = echarts.init(element, 'light');
+              // window.onresize = () => {
+              //   echart.resize();
+              // };
+              // // IE 10及以下ActiveXObject
+              // if (window.ActiveXObject) {
+              //   const WsShell = new ActiveXObject('WScript.Shell');
+              //   WsShell.SendKeys('{F11}');
+              // } else if (element.requestFullScreen) { // HTML W3C 提议
+              //   element.requestFullScreen();
+              // } else if (element.msRequestFullscreen) { // IE11
+              //   element.msRequestFullScreen();
+              // } else if (element.webkitRequestFullScreen) { // Webkit (works in Safari5.1 and Chrome 15)
+              //   element.webkitRequestFullScreen();
+              // } else if (element.mozRequestFullScreen) { // Firefox (works in nightly)
+              //   element.mozRequestFullScreen();
+              // }
             },
           },
         },
@@ -200,9 +289,7 @@ class OrgTree extends React.Component {
         title="组织结构图"
         bordered
       >
-        <div style={{ width: '100%', height: '100%' }}>
-          <div id="main" style={{ width: '100%', height: '600px', position: 'relative' }} />
-        </div>
+        <div id="main" style={{ width: '100%', height: '600px', position: 'relative', background: '#FFFFFF' }} />
       </Card>
     );
   }
